3231fe9116e9935d03e914e21df4826f3c3a9796
RBecker262 <RBecker26.2@gmail.com>
2017-04-13 08:06:22 -0500

    Cubs championship list

--- joshua2.py
+++ joshua2.py
@@ -1,13 +1,31 @@
-# Robert second program
+# Robert Second Program
+# Date: April 12, 2017
+# Work with lists to create a file of Cubs World Championships past and future
+# Added newline character to end of each record
+# Created function with established default value for passed paraameter
 
-# print('hey')
+def mainline():
 
-abc = 123
+	yearlist = [1907, 1908, 2016, 2017]
+	opponentlist = ["Tigers", "Tigers", "Indians", "Red Sox"]
+	serieslist = ["4 - 0", "4 - 1", "4 - 3", "4 - 3"]
 
-def print_stuff(stuff = 'default text'):
-	print(stuff)
+	champfile = open('ChampFile.txt', 'w')
 
+	for i in range(len(yearlist)):
+		if yearlist[i] == 2017:
+			beatit = beatstr()
+		else:
+			beatit = beatstr(yearlist[i])
+		champrec = str(yearlist[i]) + beatit + opponentlist[i] + " " + serieslist[i] + "\n"
+		champfile.write(champrec)
 
+	champfile.close()
 
-if __name__ == 'main':
-	print_stuff()
+def beatstr(whatyear = 2017):
+	if whatyear == 2017:
+		return(" Cubs will beat the ")
+	else:
+		return(" Cubs beat the ")
+
+mainline()
