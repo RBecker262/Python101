861cd6fd05db8bc1aad00eec9fb93098c956d391
RBecker262 <RBecker26.2@gmail.com>
2017-04-13 23:26:31 -0500

    changes without a branch

---
+++ cubswin.py
@@ -0,0 +1,9 @@
+# My first Python module (functions defined elsewhere)
+# Author: Robert Becker
+# Date: April 13, 2017
+# Purpose: Separating like functions into an object that can be imported into many programs (reusable code)
+
+import joshua2	# Bring in all functions in joshua2 to this program for use
+
+
+joshua2.mainline()
--- joshua2.py
+++ joshua2.py
@@ -1,4 +1,4 @@
-# My second Python program
+# My second Python program (functions only, execution of mainline is in cubswin.py)
 # Author: Robert Becker
 # Date: April 12, 2017
 # Purpose: Work with lists, file i/o, for and while loops, if/else
@@ -32,7 +32,7 @@ def mainline():
 	futurelist = []
 	i = 0
 
-	# Loop until null values (eof). Create champ string and append to list. Then add to count and read next record.
+	# Loop until null values (eof), create champ string and append to list, then add to count and read next record
 	while futureALchamp != "":
 		year = i + 2017
 		futurechampstr = str(year) + beatstr(year) + futureALchamp
@@ -42,19 +42,19 @@ def mainline():
 
 	futureALchampfile.close()
 
-	# Open Future Champs file, loop through future champs list and write each entry to file.
+	# Open output Future Champs file, loop through future champs list and write each entry to file
+	# I did not need to add the \n at the end of the record because the AL Champs input already had the newline character at the end of each input string
 	Cubsbeatfile = open('../DataFiles/FutureChampFile.txt', 'w')
 
-	for y in range(0,len(futurelist)):
-		Cubsbeatfile.write(futurelist[y])
+	for y in futurelist:
+		Cubsbeatfile.write(y)
 
 	Cubsbeatfile.close()
 
 def beatstr(whatyear = 2017):
+
 	# Set default to 2017 if not provided. If future year string should include "will"
 	if whatyear >= 2017:
 		return(" Cubs will beat the ")
 	else:
 		return(" Cubs beat the ")
-
-mainline()
